1131e5c77328d1231513a8c73daa4f89194dc5dc
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-03-02 12:30:38 +0100

    Aggiunto il quarto esercizio

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -1,1 +1,14 @@
-
+// JSnack 4
+var listaNomi = ["Giovanni", "Gianluca", "Luigi", "Alessandro", "Anna", "Matteo"];
+var invitato = false;
+var nome = prompt("Inserisci il tuo nome: ");
+for (var i = 0; i <= listaNomi.length; i++) {
+  if (nome == listaNomi[i]) {
+    invitato = true;
+  }
+}
+if (invitato == true) {
+  alert("Puoi entrare.");
+} else {
+  alert("Non sei in lista.");
+}
