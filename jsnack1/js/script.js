1a51afbb8254d9436d3a601c7c36003cf1203738
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-03-02 11:13:16 +0100

    Create cartelle e aggiunto il primo esercizio

---
+++ jsnack1/js/script.js
@@ -0,0 +1,10 @@
+// JSnack 1
+var num1 = prompt("Inserisci il primo numero: ");
+var num2 = prompt("Inserisci il secondo numero: ");
+if (num1 > num2) {
+  alert("Il primo numero che hai inserito è il maggiore!");
+} else if (num1 < num2) {
+  alert("Il secondo numero che hai inserito è il maggiore!");
+} else {
+  alert("I due numeri inseriti sono uguali!");
+}
