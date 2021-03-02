39258fb4fb3677292b4e9c96332619a87db72b6f
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-03-02 13:39:10 +0100

    Aggiunto il nono esercizio

--- jsnack9/js/script.js
+++ jsnack9/js/script.js
@@ -1,1 +1,7 @@
-
+// JSnack 9
+var somma = 0;
+for (var i = 1; i <= 10; i++) {
+  somma = somma + i;
+}
+var media = (somma / 10);
+alert("La somma dei primi 10 numeri è " + somma + " e la loro media è " + media);
