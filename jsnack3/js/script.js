11af00f5cc2ba18250c9c78d4af0c07a46a624e4
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-03-02 12:00:35 +0100

    Aggiunto il terzo esercizio

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,1 +1,11 @@
-
+// JSnack 3
+var somma = 0;
+for (var i = 1; i <= 10; i++) {
+  var num = parseInt(prompt("Inserisci un numero: "));
+  if (isNaN(num)) {
+    somma += 0;
+  } else {
+    somma = somma + num;
+  }
+}
+alert("La somma di tutti i numeri è: " + somma)
