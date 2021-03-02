ca0086fb7500edf98b5f56f3b078dcd15b9bf420
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-03-02 11:33:24 +0100

    Aggiunto il secondo esercizio

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -1,1 +1,10 @@
-
+// JSnack 2
+var parola1 = prompt("Inserisci la prima parola: ");
+var parola2 = prompt("Inserisci la seconda parola: ");
+if (parola1.length > parola2.length) {
+  alert(parola2 + " " + parola1);
+} else if (parola1.length < parola2.length) {
+  alert(parola1 + " " + parola2);
+} else {
+  alert("Le due parole inserite hanno la stessa lunghezza");
+}
