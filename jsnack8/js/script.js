66664915a96590db04f4dc3420d3607a2ec7a2cd
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-03-02 13:38:17 +0100

    Aggiunto l'ottavo esercizio

--- jsnack8/js/script.js
+++ jsnack8/js/script.js
@@ -1,1 +1,7 @@
-
+// JSnack 8
+var numero = prompt("Inserisci un numero a quattro cifre: ");
+var somma = 0;
+for (var i = 0; i < numero.length; i++) {
+  somma = somma + parseInt(numero[i]);
+}
+alert("La somma delle quattro cifre del numero è: " + somma);
