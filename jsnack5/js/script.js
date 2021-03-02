0c719ee7c975aedd8a90b8f9414e903425c36596
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-03-02 12:52:23 +0100

    Aggiunto il quinto esercizio

--- jsnack5/js/script.js
+++ jsnack5/js/script.js
@@ -1,1 +1,9 @@
-
+// JSnack 5
+var numeriDispari = [];
+for (var i = 1; i <= 6; i++) {
+  var numero = prompt("Inserisci un numero: ");
+  if ((numero % 2) != 0) {
+    numeriDispari.push(numero);
+  }
+}
+alert(numeriDispari);
